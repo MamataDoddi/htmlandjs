6bc357b2e7502948820e07b1f53795de0ffc1ec4
Mamata Doddi <mamata.rd2011@gmail.com>
2021-03-05 16:55:12 +0530

    second commit

--- function.js
+++ function.js
@@ -6,3 +6,21 @@ function myfunc()
 }
 
 myfunc()
+
+function sum(num1,num2)
+{
+	var sum=num1+num2
+	return sum
+}
+
+console.log(sum(4,8))
+
+sum(12,20)
+
+function avg(num1,num2)
+{
+	var avg=(num1+num2)/2
+	console.log(avg)
+}
+
+avg(20,30)
