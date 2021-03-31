4c9bfb6806fabff4027c66c506d9ab3bcb3a9f8d
Mamata Doddi <mamata.rd2011@gmail.com>
2021-03-31 13:25:54 +0530

    first commit

---
+++ conditionalscript.js
@@ -0,0 +1,49 @@
+// for(var i=0;i<5;i++){
+//     console.log("Hello")
+// }
+// i=0
+// while(i<5){
+//     console.log("hello")
+//     i=i+1
+// }
+
+for(var x=1;x<26;x++){
+    if(x%2!=0){
+        console.log(x)
+    }
+}
+x=0
+while(x<26){
+    if(x%2!=0){
+        console.log(x) 
+    }
+}
+
+
+
+
+
+
+// var word="ABABABABABABAB"
+// for(var i=1;i<word.length;i=i+2){
+//     console.log(word[i])
+// }
+
+// console.log("for here")
+// for(var num=0; num<5; num+=1){
+//     console.log("Number is: "+num)
+// }
+// console.log("While here")
+// var i=0
+// while(i<5){
+//     console.log(i)
+//     i+=1
+//     break
+// }
+
+// var word="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+// for(var i=0;i<word.length;i++){
+//     if (i%2==0){
+//     console.log(word[i])
+// }
+// }
---
+++ myscript.js
@@ -0,0 +1,5 @@
+var pounds=prompt("What is the weight in pounds")
+var Kg=pounds*0.454
+alert("The weight in Kg:"+kg+" Killo Grams")
+
+console.log("Convertion coleted")
